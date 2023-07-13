36b2ba9a683a980db76e644cb83d70224114a0e3
Gustav Kånåhols <gustavkanahols@gmail.com>
2023-07-13 16:30:26 +0200

    Add mock-up weather screen

--- weather/lib/routes/home.dart
+++ weather/lib/routes/home.dart
@@ -9,6 +9,236 @@ class WeatherPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var location = context.select((WeatherProvider p) => p.location);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(location),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () => context.go("/search"),
+          )
+        ],
+      ),
+      drawer: Drawer(
+        clipBehavior: Clip.antiAlias,
+        elevation: 0,
+        child: ListView(
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
+              ),
+              child: const Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Icon(
+                      Icons.wb_sunny,
+                      size: 50,
+                    ),
+                  ),
+                  Text(
+                    "Weather",
+                    style: TextStyle(
+                      fontSize: 30,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            ListTile(
+              title: const Text("About"),
+              leading: const Icon(Icons.info),
+              onTap: () => context.go("/about"),
+            ),
+          ],
+        ),
+      ),
+      body: const DefaultTabController(
+        length: 2,
+        child: Flex(
+          direction: Axis.vertical,
+          children: [
+            TabBar(
+              tabs: [
+                Tab(
+                  text: "Weather",
+                ),
+                Tab(
+                  text: "Forecast",
+                ),
+              ],
+            ),
+            Expanded(
+              child: TabBarView(
+                children: [
+                  Weather(),
+                  Forecast(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Forecast extends StatelessWidget {
+  const Forecast({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
     return const Placeholder();
+  }
+}
+
+class Weather extends StatefulWidget {
+  const Weather({
+    super.key,
+  });
+
+  @override
+  State<Weather> createState() => _WeatherState();
+}
+
+class _WeatherState extends State<Weather> {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: RefreshIndicator(
+        onRefresh: () {
+          return Future.delayed(
+            const Duration(seconds: 1),
+            () {
+              return;
+            },
+          );
+        },
+        child: ListView(
+          scrollDirection: Axis.vertical,
+          children: const [
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Flexible(
+                        flex: 1,
+                        child: Text(
+                          "13 July, 13:48",
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Flexible(
+                        flex: 1,
+                        child: Center(
+                          child: FittedBox(
+                            fit: BoxFit.fitWidth,
+                            child: Column(
+                              children: [
+                                Text(
+                                  "20C°",
+                                  style: TextStyle(
+                                    fontSize: 50,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  "Feels like 20C°",
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Flexible(
+                        flex: 1,
+                        child: WeatherAnimation(
+                          assetPath: "assets/weather/fill/clear-day.json",
+                          title: "Sunny",
+                          width: 120,
+                          height: 120,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Flexible(
+                        flex: 1,
+                        child: Text(
+                          "The whole day it's pretty much gonna be hot.",
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class WeatherAnimation extends StatelessWidget {
+  const WeatherAnimation({
+    super.key,
+    required this.assetPath,
+    required this.title,
+    required this.width,
+    required this.height,
+  });
+  final String assetPath;
+  final String title;
+  final double width;
+  final double height;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Lottie.asset(
+          assetPath,
+          width: width,
+          height: height,
+        ),
+        Center(
+          child: Text(
+            title,
+            style: const TextStyle(
+              fontSize: 20,
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
