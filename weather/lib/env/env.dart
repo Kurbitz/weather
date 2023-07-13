e9e7734e8008601f307948519ecb85409b26140e
Gustav Kånåhols <gustavkanahols@gmail.com>
2023-07-13 20:01:29 +0200

    Add API key via .env file

---
+++ weather/lib/env/env.dart
@@ -0,0 +1,9 @@
+import 'package:envied/envied.dart';
+
+part 'env.g.dart';
+
+@Envied(path: ".env")
+abstract class Env {
+  @EnviedField(varName: "OPENWEATHERMAP_API_KEY", obfuscate: true)
+  static const String OPENWEATHERMAP_API_KEY = _Env.OPENWEATHERMAP_API_KEY;
+}
