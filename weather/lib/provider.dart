86cb804fdbc21152a0bbced51bce78d92a2d958f
Gustav Kånåhols <gustavkanahols@gmail.com>
2023-07-14 15:57:45 +0200

    Add logic to store and retrieve favorites

--- weather/lib/provider.dart
+++ weather/lib/provider.dart
@@ -1,4 +1,5 @@
 import "dart:async";
+import "dart:convert";
 import "package:flutter/material.dart";
 import "package:geocoding/geocoding.dart";
 import "package:geolocator/geolocator.dart";
@@ -7,6 +8,7 @@ import "package:weather/location.dart";
 import "package:intl/intl.dart";
 import "weather.dart";
 import "openweathermap.dart";
+import "package:shared_preferences/shared_preferences.dart";
 
 class WeatherProvider extends ChangeNotifier {
   WeatherLocation _currentLocation = WeatherLocation(
@@ -14,11 +16,12 @@ class WeatherProvider extends ChangeNotifier {
     longitude: 18.07,
     name: "Stockholm",
   );
-
+  static const _favoritesKey = "favorites";
   DateTime _lastUpdated;
   final _formatter = DateFormat("d MMMM HH:mm");
   WeatherData? weatherData;
   late OpenWeatherMap _openWeatherMap;
+  List<WeatherLocation> favorites = [];
 
   String get lastUpdatedString => _formatter.format(_lastUpdated);
   String get location =>
@@ -27,6 +30,9 @@ class WeatherProvider extends ChangeNotifier {
 
   WeatherProvider() : _lastUpdated = DateTime.now() {
     _openWeatherMap = OpenWeatherMap(Env.OPENWEATHERMAP_API_KEY);
+    getSavedFavorites().then((value) {
+      favorites = value;
+    });
     update();
     // Update every 10 minutes
     Timer.periodic(const Duration(minutes: 10), (timer) {
@@ -86,5 +92,47 @@ class WeatherProvider extends ChangeNotifier {
     }
 
     setLocation(position, placemark);
+  }
+
+  Future<List<WeatherLocation>> getSavedFavorites() async {
+    final prefs = await SharedPreferences.getInstance();
+    List<String> savedFavorites;
+    try {
+      savedFavorites = prefs.getStringList(_favoritesKey) ?? [];
+    } catch (e) {
+      print(e);
+      savedFavorites = [];
+    }
+    return savedFavorites.map((e) => WeatherLocation.fromJson(jsonDecode(e))).toList();
+  }
+
+  Future<bool> favoritesAreSynced() async {
+    final savedFavorites = await getSavedFavorites();
+
+    if (savedFavorites.length != favorites.length) {
+      return false;
+    }
+
+    for (var i = 0; i < favorites.length; i++) {
+      if (!savedFavorites.contains(favorites[i])) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  void saveFavorites() async {
+    final prefs = await SharedPreferences.getInstance();
+    final favoritesJson = favorites.map((e) => jsonEncode(e.toJson())).toList();
+    prefs.setStringList(_favoritesKey, favoritesJson);
+  }
+
+  void addFavorite(WeatherLocation location) async {
+    if (!await favoritesAreSynced()) {
+      favorites = await getSavedFavorites();
+    }
+    favorites.add(location);
+    notifyListeners();
+    saveFavorites();
   }
 }
