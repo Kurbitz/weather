c853f2d306b85ced9a83971934189db4b05de5e8
Gustav Kånåhols <gustavkanahols@gmail.com>
2023-07-27 15:23:54 +0200

    Ignore const variable warning

--- weather/lib/env/env.dart
+++ weather/lib/env/env.dart
@@ -5,5 +5,6 @@ part 'env.g.dart';
 @Envied(path: ".env")
 abstract class Env {
   @EnviedField(varName: "OPENWEATHERMAP_API_KEY", obfuscate: true)
+  // ignore: non_constant_identifier_names
   static String OPENWEATHERMAP_API_KEY = _Env.OPENWEATHERMAP_API_KEY;
 }
