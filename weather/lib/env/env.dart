8fecc67f1683101299da061c3e9da0149c4c9f37
Gustav Kånåhols <gustavkanahols@gmail.com>
2023-07-27 15:18:42 +0200

    Remove const from API key

--- weather/lib/env/env.dart
+++ weather/lib/env/env.dart
@@ -5,5 +5,5 @@ part 'env.g.dart';
 @Envied(path: ".env")
 abstract class Env {
   @EnviedField(varName: "OPENWEATHERMAP_API_KEY", obfuscate: true)
-  static const String OPENWEATHERMAP_API_KEY = _Env.OPENWEATHERMAP_API_KEY;
+  static String OPENWEATHERMAP_API_KEY = _Env.OPENWEATHERMAP_API_KEY;
 }
