84bf822464021560523edb431ef7bb2b4e7a5509
Gustav Kånåhols <gustavkanahols@gmail.com>
2023-07-27 14:16:01 +0200

    Use ListView and add license/sourcecode

--- weather/lib/routes/about.dart
+++ weather/lib/routes/about.dart
@@ -20,70 +20,76 @@ class AboutPage extends StatelessWidget {
             onPressed: () => context.go("/"),
           ),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              FittedBox(
-                fit: BoxFit.fitWidth,
-                child: Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Icon(
-                        Icons.wb_sunny,
-                        size: 50,
-                        color: Theme.of(context).colorScheme.primary,
-                      ),
+        body: ListView(
+          children: [
+            FittedBox(
+              fit: BoxFit.scaleDown,
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Icon(
+                      Icons.wb_sunny,
+                      size: 50,
+                      color: Theme.of(context).colorScheme.primary,
                     ),
-                    Text(
-                      "Weather",
-                      style: Theme.of(context).textTheme.displaySmall,
-                    ),
-                  ],
+                  ),
+                  Text(
+                    "Weather",
+                    style: Theme.of(context).textTheme.displaySmall,
+                  ),
+                ],
+              ),
+            ),
+            const ListTile(
+              leading: Icon(Icons.info),
+              title: Text("Version"),
+              subtitle: Text("1.0.0"),
+            ),
+            const ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Created by"),
+              subtitle: Text("github.com/Kurbitz"),
+            ),
+            const ListTile(
+              leading: Icon(Icons.code),
+              title: Text("Source code"),
+              subtitle: Text("github.com/Kurbitz/weather"),
+            ),
+            const ListTile(
+              leading: Icon(Icons.gavel),
+              title: Text("License"),
+              subtitle: Text("GNU GPLv3"),
+            ),
+            const ListTile(
+              leading: Icon(Icons.attribution),
+              title: Text("Attribution"),
+            ),
+            Column(
+              children: [
+                Image.asset(
+                  "assets/openweather.png",
+                  width: 200,
                 ),
-              ),
-              const ListTile(
-                leading: Icon(Icons.info),
-                title: Text("Version"),
-                subtitle: Text("1.0.0"),
-              ),
-              const ListTile(
-                leading: Icon(Icons.person),
-                title: Text("Created by"),
-                subtitle: Text("github.com/Kurbitz"),
-              ),
-              const ListTile(
-                leading: Icon(Icons.attribution),
-                title: Text("Attribution"),
-              ),
-              Column(
-                children: [
-                  Image.asset(
-                    "assets/openweather.png",
-                    width: 200,
-                  ),
-                  Text(
-                    "Weather data provided by OpenWeather",
-                    textAlign: TextAlign.center,
-                    style: Theme.of(context).textTheme.bodySmall,
-                  ),
-                ],
-              ),
-              Column(
-                children: [
-                  const WeatherAnimation(
-                      assetPath: "assets/weather/fill/clear-day.json", width: 100, height: 100),
-                  Text(
-                    "Meteocons by Bas Milius",
-                    textAlign: TextAlign.center,
-                    style: Theme.of(context).textTheme.bodySmall,
-                  ),
-                ],
-              ),
-            ],
-          ),
+                Text(
+                  "Weather data provided by OpenWeather",
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.bodySmall,
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                const WeatherAnimation(
+                    assetPath: "assets/weather/fill/clear-day.json", width: 100, height: 100),
+                Text(
+                  "Meteocons by Bas Milius",
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.bodySmall,
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
